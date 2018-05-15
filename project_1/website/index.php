9a6042a5e18e45274237f7f6d030e679f3175c2b
Hypriot Pirate <pirate@black-pearl.localdomain>
2018-05-15 20:53:42 +0000

    anpassungen

--- project_1/website/index.php
+++ project_1/website/index.php
@@ -5,6 +5,9 @@
     <title>Title</title>
 </head>
 <body>
-<h1>Herzlich Willkomen</h1>
+<h1>Herzlich Willkommen</h1>
+<?php
+echo 'Hello World';
+?>
 </body>
 </html>
--- project_1/website/test.php
+++ project_1/website/test.php
@@ -5,17 +5,4 @@
  * Date: 27.10.17
  * Time: 23:39
  */
-$conn_string = "host=project-data port=5432 dbname=app user=app password=app";
-$dbconn4 = pg_connect($conn_string);
-
-
-$result = pg_query($dbconn4, "select * from auth_user");
-var_dump(pg_fetch_all($result));
-
-
-$conn_string = "host=project-data1 port=5432 dbname=app user=app password=app";
-$dbconn4 = pg_connect($conn_string);
-
-
-$result = pg_query($dbconn4, "select * from auth_user");
-var_dump(pg_fetch_all($result));
+phpinfo();
